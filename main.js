ca36d1e554c663bcdae4ab01294819141522862d
latenightsnacc <egonubuchi97@gmail.com>
2020-04-25 16:42:02 -0400

    Edited the functions in main.js. Edited the index.js. Edited the style.scss file.

--- main.js
+++ main.js
@@ -16,11 +16,29 @@ let msg = document.getElementById('card-message');
 let sign = document.getElementById('card-sign');
 let sender = document.getElementById('sender');
 
-// CARD (OUTPUT)
-let cardTitle = document.querySelector('.cardTitle');
-let cardMsg = document.querySelector('.cardText');
-let cardSign = document.querySelector('#greeting');
-let cardSender = document.querySelector('#senderName');
+// FAMILY CARD (OUTPUT)
+let fmCt = document.querySelector('#fmCT');
+let fmCtxt = document.querySelector('#fmCTxt');
+let fmSg = document.querySelector('#greetingFm');
+let fmSn = document.querySelector('#senderFm');
+
+// FRIEND CARD (OUTPUT)
+let frCt = document.querySelector('#frCT');
+let frCtxt = document.querySelector('#frCTxt');
+let frSg = document.querySelector('#greetingFr');
+let frSn = document.querySelector('#senderFr');
+
+// LOVER CARD (OUTPUT)
+let lrCt = document.querySelector('#lrCT');
+let lrCtxt = document.querySelector('#lrCTxt');
+let lrSg = document.querySelector('#greetingLr');
+let lrSn = document.querySelector('#senderLr');
+
+// FRENEMY CARD (OUTPUT)
+let freCt = document.querySelector('#freCT');
+let freCtxt = document.querySelector('#freCTxt');
+let freSg = document.querySelector('#greetingFre');
+let freSn = document.querySelector('#senderFre');
 
 // CLICK EVENTS
 familyBtn.addEventListener('click', displayFamilyCard);
@@ -80,35 +98,68 @@ function displayFrenemyCard(e){
 }
 
 
+// This function writes the card title inputted in the form unto the card element.
 function writeTitle(e){
     //console.log(e.target.value);
     if (familyCard.style.display === "block"){
-        cardTitle.textContent = e.target.value;
+        fmCt.textContent = e.target.value;
     } 
-     else if(friendCard.style.display === "block"){
-        cardTitle.textContent = e.target.value;
+    if(friendCard.style.display === "block"){
+        frCt.textContent = e.target.value;
     } 
-    else if(loverCard.style.display === "block"){
-        cardTitle.textContent = e.target.value;
+    if(loverCard.style.display === "block"){
+        lrCt.textContent = e.target.value;
     }
-    else if(frenemyCard.style.display === "block"){
-        cardTitle.textContent = e.target.value;
+    if(frenemyCard.style.display === "block"){
+        freCt.textContent = e.target.value;
     }
     
 }
 
+// This function writes the card message inputted in the form unto the card element.
 function writeMsg(e){
-    //console.log(e.target.value);
-    cardMsg.textContent = e.target.value;
+    if (familyCard.style.display === "block"){
+        fmCtxt.textContent = e.target.value;
+    } 
+    if(friendCard.style.display === "block"){
+        frCtxt.textContent = e.target.value;
+    } 
+    if(loverCard.style.display === "block"){
+        lrCtxt.textContent = e.target.value;
+    }
+    if(frenemyCard.style.display === "block"){
+        freCtxt.textContent = e.target.value;
+    }
 }
 
+// This function writes the card signature inputted in the form unto the card element.
 function writeSign(e){
-    //console.log(e.target.value);
-    cardSign.textContent = e.target.value;
+    if (familyCard.style.display === "block"){
+        fmSg.textContent = e.target.value;
+    } 
+    if(friendCard.style.display === "block"){
+        frSg.textContent = e.target.value;
+    } 
+    if(loverCard.style.display === "block"){
+        lrSg.textContent = e.target.value;
+    }
+    if(frenemyCard.style.display === "block"){
+        freSg.textContent = e.target.value;
+    }
 }
 
+// This function writes the sender's name inputted in the form unto the card element.
 function writeName(e){
-    //console.log(e.target.value);
-    cardSender.textContent = e.target.value;
+    if (familyCard.style.display === "block"){
+        fmSn.textContent = e.target.value;
+    } 
+    if(friendCard.style.display === "block"){
+        frSn.textContent = e.target.value;
+    } 
+    if(loverCard.style.display === "block"){
+        lrSn.textContent = e.target.value;
+    }
+    if(frenemyCard.style.display === "block"){
+        freSn.textContent = e.target.value;
+    }
 }
-
